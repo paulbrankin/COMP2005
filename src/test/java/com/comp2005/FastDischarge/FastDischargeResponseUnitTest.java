a8e6609856e5d5db1a646ebc3ebd5fad750c8676
Paul Brankin <paul.b.brankin@oracle.com>
2023-05-21 12:38:18 +0100

    Fixed

--- src/test/java/com/comp2005/FastDischarge/FastDischargeResponseUnitTest.java
+++ src/test/java/com/comp2005/FastDischarge/FastDischargeResponseUnitTest.java
@@ -45,7 +45,7 @@ class FastDischargeResponseUnitTest {
         String actualJsonString = objectMapper.writeValueAsString(response);
 
         // Convert the expected JSON string to a JSON object
-        JsonNode expectedJson = objectMapper.readTree("{\"fastDisciargeArray\":[{\"patientId\":1,\"forename\":\"John\",\"surname\":\"Doe\",\"nhsnumber\":\"123456789\",\"admissionDate\":\"2022-01-01\",\"dischargeDate\":\"2022-01-02\",\"durationHours\":24},{\"patientId\":2,\"forename\":\"Jane\",\"surname\":\"Smith\",\"nhsnumber\":\"987654321\",\"admissionDate\":\"2022-02-01\",\"dischargeDate\":\"2022-02-02\",\"durationHours\":24}]}");
+        JsonNode expectedJson = objectMapper.readTree("{\"fastDischargeArray\":[{\"patientId\":1,\"forename\":\"John\",\"surname\":\"Doe\",\"nhsnumber\":\"123456789\",\"admissionDate\":\"2022-01-01\",\"dischargeDate\":\"2022-01-02\",\"durationHours\":24},{\"patientId\":2,\"forename\":\"Jane\",\"surname\":\"Smith\",\"nhsnumber\":\"987654321\",\"admissionDate\":\"2022-02-01\",\"dischargeDate\":\"2022-02-02\",\"durationHours\":24}]}");
 
         // Convert the actual JSON string to a JSON object
         JsonNode actualJson = objectMapper.readTree(actualJsonString);
