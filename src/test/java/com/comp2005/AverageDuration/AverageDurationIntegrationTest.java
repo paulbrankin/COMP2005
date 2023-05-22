1695088154fd9d0e1280bb02d7bf673dcfb47a4e
Paul Brankin <paul.b.brankin@oracle.com>
2023-05-22 20:52:55 +0100

    some new change

--- src/test/java/com/comp2005/AverageDuration/AverageDurationIntegrationTest.java
+++ src/test/java/com/comp2005/AverageDuration/AverageDurationIntegrationTest.java
@@ -42,7 +42,7 @@ public class AverageDurationIntegrationTest {
         String allocationResponse = "[{\"id\":99,\"admissionID\":1,\"employeeID\":4,\"startTime\":\"2020-11-28T16:45:00\",\"endTime\":\"2020-11-28T23:56:00\"}," +
                 "{\"id\":2,\"admissionID\":3,\"employeeID\":4,\"startTime\":\"2021-09-23T21:50:00\",\"endTime\":\"2021-09-24T09:50:00\"}," +
                 "{\"id\":98,\"admissionID\":6,\"employeeID\":4,\"startTime\":\"2021-09-23T21:50:00\",\"endTime\":\"2021-09-24T09:50:00\"}]";
-
+//change
         String admissionResponse = "[{\"id\":1,\"admissionDate\":\"2020-11-28T16:45:00\",\"dischargeDate\":\"2020-11-28T23:56:00\",\"patientID\":2}," +
                 "{\"id\":2,\"admissionDate\":\"2020-12-07T22:14:00\",\"dischargeDate\":\"0001-01-01T00:00:00\",\"patientID\":1}," +
                 "{\"id\":3,\"admissionDate\":\"2021-09-23T21:50:00\",\"dischargeDate\":\"2021-09-27T09:56:00\",\"patientID\":2}," +
